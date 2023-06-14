9a4c0c5919a4058e73f7df8656fb2388516f1189
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-14 15:04:49 -0500

    finished servlets with JSPs exercises

--- src/main/java/GuessIncorrectServlet.java
+++ src/main/java/GuessIncorrectServlet.java
@@ -1,2 +1,14 @@
-public class GuessIncorrectServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "GuessIncorrectServlet", urlPatterns = "/incorrect")
+public class GuessIncorrectServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setAttribute("outcome", "That was a wrong guess");
+        request.getRequestDispatcher("guessResult.jsp").forward(request, response);
+    }
 }
