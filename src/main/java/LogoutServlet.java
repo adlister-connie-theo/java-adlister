a5c459507af607a0345c55ed525536825288c3d5
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-20 09:07:26 -0500

    finished exercises

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -6,17 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        String isLogged = (String) session.getAttribute("user");
-        if(isLogged == "admin") {
-            request.getRequestDispatcher("/profile.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("/login");
-        }
+        session.removeAttribute("user");
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+
 
 
 
