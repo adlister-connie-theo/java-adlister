08c13eeca9f3f70d30869c987b0088018acd81d4
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-26 09:35:37 -0500

    user input exercises

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -1,12 +1,20 @@
 package com.codeup.adlister.models;
 
+import lombok.ToString;
+
+@ToString
+
 public class User {
     private long id;
     private String username;
     private String email;
     private String password;
 
-    public User() {}
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public User(long id, String username, String email, String password) {
         this.id = id;
